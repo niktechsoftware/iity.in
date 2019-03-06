4719ac2915b508bcb6b80e6e7af977b1e127dabb
niktechsoftware <info@niktechsoftware.com>
2019-03-06 01:57:34 -0800

    correct all file

--- application/views/vision.php
+++ application/views/vision.php
@@ -11,9 +11,9 @@
     <?php $this->load->view('header'); ?>   
 	 <?php $this->load->view('menu'); ?> 
  <div class="container-fluid jumbotron col-md-12" style="margin-top:-40px">
-	 	<center><h3>Vision</h3></center>
+	 	<center><h3>VISION</h3></center>
 	 <center><h2>Institute of Information Technology & Yoga</h2><center>
-	 <center><p style="text-align: justify;">To work for the common man to avail best quality technical education To build a state-of-the art global computer training infrastructure with a wide range of network spread across the region channel partners.
+	 <center><p style="text-align: justify;">To work for the common men to avail best quality technical education, To build a state-of-the art global computer training infrastructure with a wide range of network spread across the region channel partners.
      </p>
      <center><p style="text-align: justify;">To promote excellence in education for a vibrant and inclusive society through knowledge creation and dissemination.
      </p>
